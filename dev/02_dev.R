0670d7d7e673015326bb6de980b133a06a76d9c3
Eric Nantz <thercast@gmail.com>
2023-10-14 14:39:52 -0400

    update dev script

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -21,11 +21,12 @@ attachment::att_amend_desc()
 ## Add modules ----
 ## Create a module infrastructure in R/
 golem::add_module(name = "auth_info", with_test = FALSE)
+golem::add_module(name = "puzzle_viewer", fct = "helpers", with_test = FALSE)
 golem::add_module(name = "name_of_module2", with_test = TRUE) # Name of the module
 
 ## Add helper functions ----
 ## Creates fct_* and utils_*
-golem::add_fct("helpers", with_test = TRUE)
+golem::add_fct("database", with_test = FALSE)
 golem::add_utils("helpers", with_test = TRUE)
 
 ## External resources
