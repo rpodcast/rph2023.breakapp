d7169b31e4936e6f177d64e74ef9b474e08638e9
Eric Nantz <thercast@gmail.com>
2023-10-14 14:34:30 -0400

    add ui utility functions

--- dev/01_start.R
+++ dev/01_start.R
@@ -59,7 +59,7 @@ golem::use_favicon() # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
-golem::use_utils_ui(with_test = TRUE)
+golem::use_utils_ui(with_test = FALSE)
 golem::use_utils_server(with_test = TRUE)
 
 # You're now set! ----
