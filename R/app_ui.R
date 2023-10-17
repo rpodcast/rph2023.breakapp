d2b94b4f206d87d5cbf6bea15bd3704ef01efa1a
Eric Nantz <thercast@gmail.com>
2023-10-17 18:17:40 +0000

    declare usage of shinypop in ui

--- R/app_ui.R
+++ R/app_ui.R
@@ -79,8 +79,9 @@ golem_add_external_resources <- function() {
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "rph2023.breakapp"
-    )
+    ),
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
+    shinypop::use_noty(maxVisible = 3)
   )
 }
